6c8892bf2b34d51ee1d3c1ab959196d046ed1ba9
Aninsi Sasberg <Maximilian.Bernhard@vodafone.de>
2022-08-02 03:38:47 +0200

    feat: add basic structure of loading everything before loading the GUI or opening the Terminal

---
+++ bin/annotate/load.py
@@ -0,0 +1,14 @@
+# loads all Notebooks, and opens them as rw
+
+# load settings from res/filesystem/settings.xml
+settingx = "lala"
+
+# load recent files from res/filesystem/data.xml
+recentsPath = "lala"
+
+# read from res/filesystem/notebooks.xml
+notebooksPath = "uiiui"
+
+
+'''think about linking recent files with Notebook/List loading'''
+
