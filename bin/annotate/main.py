681b9b20f2271e8bfb83dd05bed3e98b78f5f7b5
Aninsi Sasberg <Maximilian.Bernhard@vodafone.de>
2022-08-24 17:48:48 +0200

    fix: move File to files.py and add window method call

--- bin/annotate/main.py
+++ bin/annotate/main.py
@@ -1,25 +1,5 @@
-# needed modes:
-# r for reading only
-# w for writing on/over the file, creating new one if doesn't exist
-# ↑ (rw) standard mode
+import os
+import gui
+import terminal as term
 
-
-class File:
-
-    openedFileList = []
-
-    def __init__(self, path, mode, filetype):
-        self.path = path
-        self.mode = mode
-        self.filetype = filetype
-        self.openedFileList.append(self)
-
-    def openFile(self, file):
-        self.file = file = open(self.path, self.mode)
-        return file
-
-    def saveToFile(self, input):
-        input = self.file.append()
-
-file1 = File(".\ai\ai.txt", "wr", ".txt")
-print(file1)
+gui.window()
